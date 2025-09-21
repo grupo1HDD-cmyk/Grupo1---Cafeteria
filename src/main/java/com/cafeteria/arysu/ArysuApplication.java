32827a19d2e22898ce819bcb471ffcdd4c33e71d
IsaacUlloa <isaaculloamucha@gmail.com>
2025-09-21 05:02:55 -0500

    Se sube el proyecto Java

---
+++ src/main/java/com/cafeteria/arysu/ArysuApplication.java
@@ -0,0 +1,13 @@
+package com.cafeteria.arysu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArysuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArysuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafeteria/arysu/ArysuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafeteria.arysu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArysuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
